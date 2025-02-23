e2a29555b7e63eae62276c6b08772dc77567489d
Dominic Go <dominic@dominicgo.dev>
2025-02-24 07:21:44 +0800

    💫 Update: App Entry

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { PokemonAPI } from './src/services/PokemonAPI';
+import usePokemonData from './src/hooks/UsePokemonData';
 
 export default function App() {
+  const { isLoading } = usePokemonData();
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
